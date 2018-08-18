814fc622cbd92cacf352d4b79d16f072830a16b1
volyx <volyihin@gmail.com>
2018-08-18 09:16:55 +0300

    add jlink to javapackager

---
+++ javalackager.cmd
@@ -0,0 +1,10 @@
+rm -r -f release
+javapackager -deploy -Bruntime=/Library/Java/JavaVirtualMachines/jdk-10.0.2.jdk/Contents/Home \
+-native image \
+-BjlinkOptions=compress=2 \
+-outdir release -outfile Klikr.app \
+--module-path /Users/volyx/Projects/klikr/klikr/target \
+--module io.github.volyx.klikr/io.github.volyx.klikr.Main \
+-name "Klikr" \
+-title "Klikr" \
+-Bverbose=true
